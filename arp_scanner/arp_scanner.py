c6a4eb3a5965d294c8314e8122dea61e3e429913
tripix404 <tripix404@users.noreply.github.com>
2025-05-06 08:07:48 +0200

    while true stuk voor de target_ip functie

--- arp_scanner/arp_scanner.py
+++ arp_scanner/arp_scanner.py
@@ -8,6 +8,7 @@ from pathlib import path
 import platform
 
 def sace_results(data, filename="scan_results"):
+    """ opslagen van resultaten in een JSON-bestand """"
 
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     safe_name = base64.b64encode(f"{filename}_{timestamp}".encode()).decode()
@@ -19,3 +20,50 @@ def sace_results(data, filename="scan_results"):
         }, f, indent=2)
     print(f"[+] resultaten opgeslagen in {path.name}")
 
+def arp_scan(target_ip="192.168.1.0/24"):
+    """scant het netwerk voor actieve devices met Scapy"""
+    print("[+]  ARP-scan uitvoeren...")
+
+    # Toon alle beschikbare interfaces
+    available_ifaces = [iface.name for iface in get_working_ifaces()]
+    print(f" [+] Beschikbare interfaces:")
+    for idx, iface in enumerate(available_ifaces):
+        print(f"    {idx}: {iface}")
+    
+    # vraag gebruiker om een interface te kiezen
+    while true:
+        try:
+            iface_idx = int(input(f"Kies het nummer om een interface te kiezen (0-{len(available_ifaces)-1}): "))
+            iface = available_ifaces[iface_idx]
+            break
+        except (ValueError, IndexError):
+            print("Ongeldige keuze, kies opnieuw;")
+        
+    print(f"[+] Gekozen interfqce: {iface}")
+    print(f"[+] Gekozen IP adres: {target_ip}")
+
+    try:
+        arp = ARP(pdst=target_ip)
+        ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+        packet = ether/arp
+        print("[DEBUG] ARP-pakket wordt verstuurd...")
+        result = srp(packet, iface=iface, timeout=5, verbose=False)[0]
+
+        devices = []
+        for sent, received in result:
+            devices.append({'IP' : received.psrc, 'MAC' : received.hwsrc})
+
+        if not devices:
+            print("[!] Geen apparaten gevonden. Mogelijke oorzaken:")
+            print("- Verkeerde interface gekozen")
+            print("- Gekozen IP netwerk komt niet overeen met eigen subnet")
+            print("- Firewall blokkeert ARP-verkeer")
+        else:
+            print(f"[+] {len(devices)} apparaten gevonden.")
+    
+        return devices
+    except Exception as e:
+        print(f"[-] Kritieke fout tijdens ARP-scan: {str(e)}")
+        print("[+] Voer het script uit als Administrator en of controleer je firewall instellingen")
+        return []
+        
