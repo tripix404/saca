65ac4d503400f2f151c0410bb04620b7c30c782a
tripix404 <tripix404@users.noreply.github.com>
2025-05-08 22:08:35 +0200

    changed banner with some colors

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 import sys
 import subprocess
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True)
 
 def clear_screen():
     """Clear the terminal screen."""
     os.system('cls' if os.name == 'nt' else 'clear')
+   
 
 def run_script(script_path):
     """Run a Python script using subprocess."""
@@ -17,40 +21,54 @@ def run_script(script_path):
         print(f"Script not found: {script_path}")
     input("\nPress Enter to return to main menu...")
 
+import os
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True)
+
 def display_menu():
     """Display the main menu of the ethical hacking toolkit."""
     clear_screen()
+    # Definieer de banner EERST
     banner = r"""
                                                  
                                                  
-.d8888b   8888b.   .d8888b  8888b.               
-88K          "88b d88P"        "88b              
-"Y8888b. .d888888 888      .d888888              
-     X88 888  888 Y88b.    888  888              
- 88888P' "Y888888  "Y8888P "Y888888              
+    .d8888b   8888b.   .d8888b  8888b.           
+    88K          "88b d88P"        "88b          
+    "Y8888b. .d888888 888      .d888888          
+         X88 888  888 Y88b.    888  888          
+    88888P' "Y888888  "Y8888P "Y888888           
                                                  
                                                  
                                                  
 888                      888 888      d8b 888    
 888                      888 888      Y8P 888    
-888                      888          888    
+888                      888 888          888     
 888888  .d88b.   .d88b.  888 888  888 888 888888 
 888    d88""88b d88""88b 888 888 .88P 888 888    
 888    888  888 888  888 888 888888K  888 888    
 Y88b.  Y88..88P Y88..88P 888 888 "88b 888 Y88b.  
- "Y888  "Y88P"   "Y88P"  888 888  888 888  "Y888
+ "Y888  "Y88P"   "Y88P"  888 888  888 888  "Y888 
 """
-    print(banner)
-    print("=" * 50)
-    print("        ETHICAL HACKING TOOLKIT - HOWEST - by Georges Devos")
-    print("=" * 50)
-    print("1. DDoS Attack Tool")
-    print("2. Web Scraper Tool")
-    print("3. MAC Spoofer Tool")
-    print("4. ARP Scanner Tool")
-    print("0. Exit")
-    print("=" * 50)
-    return input("Selecteer een tool (0-4): ")
+    width = os.get_terminal_size().columns
+
+    # Print elke regel van de banner gecentreerd en gekleurd
+    for line in banner.splitlines():
+        print(Style.BRIGHT + Fore.RED + Back.BLACK + line.center(width) + Style.RESET_ALL)
+
+    # Print de rest van het menu, ook gecentreerd als je dat mooi vindt:
+    print(Style.BRIGHT + Fore.MAGENTA + "=" * width + Style.RESET_ALL)
+    print(Style.BRIGHT + Fore.YELLOW + "ETHICAL HACKING TOOLKIT - HOWEST - by Georges Devos".center(width) + Style.RESET_ALL)
+    print(Style.BRIGHT + Fore.MAGENTA + "=" * width + Style.RESET_ALL)
+    print("1. DDoS Attack Tool".center(width))
+    print("2. Web Scraper Tool".center(width))
+    print("3. MAC Spoofer Tool".center(width))
+    print("4. ARP Scanner Tool".center(width))
+    print("0. Exit".center(width))
+    print(Style.BRIGHT + Fore.MAGENTA + "=" * width + Style.RESET_ALL)
+    return input("Selecteer een tool (0-4): ".center(width))
+
+
 
 def main():
     """Main function that runs the ethical hacking toolkit."""
